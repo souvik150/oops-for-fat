4128201193efa58b587526a0daa995572cfb1c97
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-04 10:39:20 +0530

    Done

--- SampleQP/C/q2/main.c
+++ SampleQP/C/q2/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main(){
-  int c1;
-  printf("Please enter a 4 digit number");
-  printf("\n");
-  scanf("%d", &c1);
-   
-  int c2 = c1/10000;
-  int c3 = (c1%100)/10;
- 
-  printf("%d", c2+c3);
- 
+
+int main()
+{
+  int no, first,second, third, fourth, Increased_no;
+  printf("Type any 4-digit no. : \n");
+  scanf("%d",&no);
+
+  first = no/1000;
+  no = no%1000;
+  second = no/100;
+  no = no%100;
+  third = no/10;
+  fourth = no%10;
+
+  Increased_no = (first+2)*1000+(second+2)*100+(third+2)*10+(fourth+2);
+  printf("Increased No. is %d",Increased_no);
+
   return 0;
 }
