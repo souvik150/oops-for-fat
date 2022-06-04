d44a9be0cc7bc635356c703f3b5464fe2dbbc622
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-04 10:07:32 +0530

    Made some chnages

--- SampleQP/C/q2/main.c
+++ SampleQP/C/q2/main.c
@@ -3,14 +3,14 @@
 
 int main(){
   int c1;
-  printf("Please enter a 5 digit number");
+  printf("Please enter a 4 digit number");
   printf("\n");
   scanf("%d", &c1);
-  
+   
   int c2 = c1/10000;
   int c3 = (c1%100)/10;
-
+ 
   printf("%d", c2+c3);
-
+ 
   return 0;
 }
