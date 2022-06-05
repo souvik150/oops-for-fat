9f47a2fe1c81aa9a4c95fbcf711aee7e4c6d408b
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-05 11:30:21 +0530

    Made some changes

---
+++ SampleQP/C/q21/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool areAnagram(char *str1, char *str2)
+{
+	int NO_OF_CHARS = 256;
+	int count[NO_OF_CHARS] = {0};
+	int i;
+
+	for (i = 0; str1[i] && str2[i]; i++)
+	{
+		count[str1[i]]++;
+		count[str2[i]]--;
+	}
+
+	if (str1[i] || str2[i])
+		return false;
+
+	for (i = 0; i < NO_OF_CHARS; i++)
+		if (count[i])
+			return false;
+	return true;
+}
+
+int main()
+{
+	char str1[100];
+	char str2[100];
+
+	gets(str1);
+	gets(str2);
+
+	if (areAnagram(str1, str2))
+		cout << "The two strings are anagram of each other";
+	else
+		cout << "The two strings are not anagram of each "
+						"other";
+
+	return 0;
+}
