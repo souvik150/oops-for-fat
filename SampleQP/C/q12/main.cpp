05b51f574fa9c1a9a9c43baa8d1099e9661344cf
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-06 16:51:37 +0530

    Finishing up

---
+++ SampleQP/C/q12/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 100;
+
+bool isSymmetric(int mat[][MAX], int N)
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (mat[i][j] != mat[j][i])
+				return false;
+	return true;
+}
+
+// Driver code
+int main()
+{
+	int mat[][MAX] = { { 1, 3, 5 },
+					{ 3, 2, 4 },
+					{ 5, 4, 1 } };
+
+	if (isSymmetric(mat, 3))
+		cout << "Yes";
+	else
+		cout << "No";
+	return 0;
+}
