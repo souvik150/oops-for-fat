30d908df4f0ceb79ae9f48aea14ee675d0de91ba
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-16 23:07:04 +0530

    Done

---
+++ STL/Class Templates/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+template <typename T, typename U>
+
+class Vit
+{
+private:
+  T x;
+  U y;
+
+public:
+  Vit(T z, U m)
+  {
+    x = z;
+    y = m;
+  }
+  U add()
+  {
+    return (x + y);
+  }
+};
+
+int main()
+{
+  Vit<int, int>obj1(2,3);
+  Vit<float, float>obj2(2.4, 3.4);
+  Vit<int, float>obj3(2, 3.4);
+
+  cout<<obj1.add()<<endl;
+  cout<<obj2.add()<<endl;
+  cout<<obj3.add()<<endl;
+  
+  return 0;
+}
